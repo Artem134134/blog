874c09c518796829aad4f28fb2146a07cd9b4e0d
Artem134134 <car322132@gmail.com>
2022-12-05 18:16:52 +0300

    Show username in header

--- db/migrate/20221201194345_add_username_index.rb
+++ db/migrate/20221201194345_add_username_index.rb
@@ -1,5 +1,6 @@
 class AddUsernameIndex < ActiveRecord::Migration[7.0]
   def change
-    add_index :users, :username, unique: true
+    add_column :users, :username, :string
+    add_index  :users, :username, unique: true
   end
 end
