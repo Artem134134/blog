4d143ff2abe701a6d81fa0636553d3e0756cffe2
Artem134134 <car322132@gmail.com>
2022-12-15 23:46:13 +0300

    Update create contact test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -5,6 +5,6 @@ feature "Creating Contact" do
   scenario "allows acees to contacts page" do 
     visit new_contacts_path
 
-    expect(page).to have_content 'Contact us'
-  end
+    expect(page).to have_content I18n.t('contacts.contact_us')
+  end     
 end
