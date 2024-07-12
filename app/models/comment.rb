d9516e9d471b3fe8acbcbbac6626402790899779
Artem134134 <car322132@gmail.com>
2024-07-12 12:54:45 +0300

    In the comment model, validation for presence and length has been added to the author and body fields. Added a method for retrieving the time and date a comment was created from the database.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  validates :body, length: {maximum: 100}
+  
+  validates :author, presence: true, length: {maximum: 50}
+  validates :body, presence: true, length: {maximum: 200}
+  
+
+
+  def formatted_created_on
+    created_at.strftime('pub. date %d.%m.%Y  %H:%M')
+  end
 end
