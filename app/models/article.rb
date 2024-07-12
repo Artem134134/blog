bf50cd2b500a2313f2209e4e781f07c48752f91e
Artem134134 <car322132@gmail.com>
2024-07-12 14:18:41 +0300

    Length validation has been changed in the article and comment model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,8 @@ class Article < ApplicationRecord
   # dependent: :destroy - удаление комментариев связанных со статьёй!
   has_many :comments, dependent: :destroy 
   
-  validates :title, presence: true, length: {maximum: 100}
-  validates :text, presence: true, length: {maximum: 1000}
+  validates :title, presence: true, length: {in: 2..100}
+  validates :text, presence: true, length: {in: 2..1000}
  
   def subject
     title
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :article
   
-  validates :author, presence: true, length: {maximum: 50}
-  validates :body, presence: true, length: {maximum: 200}
+  validates :author, presence: true, length: {in: 2..50}
+  validates :body, presence: true, length: {in: 2..200}
   
 
 
