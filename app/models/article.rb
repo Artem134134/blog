0a30bbc21f3a544b50428042e6d7ac590f02f813
Artem134134 <car322132@gmail.com>
2024-07-12 12:48:32 +0300

    Minor changes to the model article.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,10 @@
 class Article < ApplicationRecord
   # dependent: :destroy - удаление комментариев связанных со статьёй!
   has_many :comments, dependent: :destroy 
-
-  validates :title, presence: true
-  validates :text,  presence: true
-  validates :title, length: {maximum: 60}
-  validates :text,  length: {maximum: 1000}
+  
+  validates :title, presence: true, length: {maximum: 100}
+  validates :text, presence: true, length: {maximum: 1000}
  
-
   def subject
     title
   end
