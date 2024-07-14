598a3e0cd13e8904257e326af1d675a6f898e91f
Artem134134 <car322132@gmail.com>
2024-07-14 15:39:06 +0300

    Removed Home controller, view and path in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
    devise_for :users
   root to: "articles#index"
 
-  get '/' => 'home#index'
-
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
 
@@ -11,8 +9,5 @@ Rails.application.routes.draw do
   resources :articles do 
     resources :comments
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
