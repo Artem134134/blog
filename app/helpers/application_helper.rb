7386d64a241c311f1beac9b0fdde91fe0b77ad78
Artem134134 <car322132@gmail.com>
2024-07-16 19:05:02 +0300

    Delete method added a bootstrap alert class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,3 @@
 module ApplicationHelper
 
-  def bootstrap_alert_class(type)
-    {
-      notice: 'info',
-      info: 'info',
-      success: 'success',
-      warning: 'warning',
-      error: 'danger',
-      alert: 'danger'
-    }.fetch(type.to_sym, type.to_s)
-  end
-  
 end
