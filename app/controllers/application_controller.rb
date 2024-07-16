b1b3add8eb69cb1770013eb26c2b4417cb57cc62
Artem134134 <car322132@gmail.com>
2024-07-16 23:40:03 +0300

    Moved the notfound method to the error_handling.rb file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  rescue_from ActiveRecord::RecordNotFound, with: :notfound 
-
-  private
-
-  def notfound(exception)
-    logger.warn exception
-    render file: 'public/404.html', status: :not_found, layout: false
-  end
-
+  
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])  
   end 
-  
+
 end
