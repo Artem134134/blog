4221f2adb34869d2a136b92932a2c04779491f27
Artem134134 <car322132@gmail.com>
2024-07-18 19:49:32 +0300

    Routes refactoring.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-   devise_for :users
+  devise_for :users
+
   root to: "articles#index"
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
 
-  resource  :contacts, only: [:new, :create], path_names: {:new => ''}
+  resource  :contacts, only: %i[new create], path_names: {new: ''}
+  
   resources :articles do 
-    resources :comments
+    resources :comments, only: %i[new create]
   end
   
 end
