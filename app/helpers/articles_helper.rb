e953507466a827abb3367d081fc583a661c45c74
Artem134134 <car322132@gmail.com>
2024-08-12 00:54:47 +0300

    Truncated_text method has been moved to articles_helper.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,11 @@
 module ArticlesHelper
+	
+	def truncated_text(text, max_length = 150)
+    if text.length > max_length
+      "#{text[0...max_length]}... <small>continued</small>"
+    else
+      text
+    end
+  end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -5,11 +5,4 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: {in: 2..100}
   validates :text, presence: true, length: {in: 2..1000}
  
-   def truncated_text(max_length = 150)
-    if text.length > max_length
-      "#{text[0...max_length]}... <small>continued</small>"
-    else
-      text
-    end
-  end
 end
