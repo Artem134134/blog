7573d57feac141f1374fcc4b73d70b8dd9a46c2a
Artem134134 <car322132@gmail.com>
2024-08-11 23:05:43 +0300

    Removed from the Article model helper methods that are not used.

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,12 +5,5 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: {in: 2..100}
   validates :text, presence: true, length: {in: 2..1000}
  
-  def subject
-    title
-  end
 
-  def last_comment
-    comments.last
-  end
-  
 end
