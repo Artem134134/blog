c629a07c4c9bbb94b31c2b52e0802437a7a457b1
Artem134134 <car322132@gmail.com>
2024-08-24 16:37:19 +0300

    Добавлены методы для поддержки локализации: - Реализован метод switch_locale для переключения локали. - Добавлен метод locale_from_url для извлечения локали из URL. - Обновлен метод default_url_options для включения локали в URL.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,27 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  around_action :switch_locale
+
+  private
+
+  def switch_locale(&action)
+    locale = locale_from_url || I18n.default_locale
+    I18n.with_locale locale, &action
+  end
+
+  def locale_from_url
+    locale = params[:locale]
+
+    return locale if I18n.available_locales.map(&:to_s).include?(locale)
+  
+    nil
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   protected
 
   def configure_permitted_parameters
