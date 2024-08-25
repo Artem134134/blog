f31b8cfa09fbc3780c4fa68a237db2a54ca5273b
Artem134134 <car322132@gmail.com>
2024-08-26 01:38:13 +0300

    Добавлены методы в application helper, для вызова навигационной панели(currently_at) и метод(nav_tab) для переключения активных ссылок.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,24 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def nav_tab(title, url, options = {})
+    current_page = options.delete :current_page
+
+    css_class = current_page == title ? 'text-secondary' : 'text-white'
+
+    options[:class] = if options[:class]
+                        options[:class] + ' ' + css_class
+                      else
+                        css_class
+                      end
+    
+    link_to title, url, options
+  end
+
+  def currently_at(current_page = '')
+    render partial: 'shared/navbar', locals: {current_page: current_page}
+  end
+
   def formatted_created_at(obj)
     #obj.created_at.strftime('pub. date %d.%m.%Y  %H:%M')
     l obj.created_at, format: :long
