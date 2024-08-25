7a652179fd6b9852fd5c174f7790fdd26ecb3ff3
Artem134134 <car322132@gmail.com>
2024-08-26 01:17:44 +0300

    Использование локализации для текста продолжения в методе truncated_text

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,7 +3,7 @@
 module ArticlesHelper
   def truncated_text(text, max_length = 150)
     if text.length > max_length
-      "#{text[0...max_length]}... <small>continued</small>"
+      "#{text[0...max_length]}<small>#{I18n.t('continued')}</small>"
     else
       text
     end
