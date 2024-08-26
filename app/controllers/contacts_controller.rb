af2e521c578dd65b7aaaf39ab0cd2b9a1166b7db
Artem134134 <car322132@gmail.com>
2024-08-26 14:36:07 +0300

    Модифицировал contacts controller, добавлена проверка эл. почты на валидность

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,22 @@
 # frozen_string_literal: true
 
 class ContactsController < ApplicationController
+  include ContactsHelper
+
   def new
     @contact = Contact.new
   end
 
   def create
     @contact = Contact.new(contact_params)
-    if @contact.valid?
+
+    if @contact.valid? && valid_email?(@contact.email)
       @contact.save
       flash[:success] = t '.success'
       redirect_to root_path
     else
       flash.now[:warning] = t '.warning'
+      @invalid_email = !valid_email?(@contact.email)
       render :new
     end
   end
