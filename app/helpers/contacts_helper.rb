14373a19a19f9080960b42344f11f256aaee557a
Artem134134 <car322132@gmail.com>
2024-08-26 14:29:46 +0300

    Добавлен метод для проверки Эл. почты в contacts helper

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ContactsHelper
+	def valid_email?(email)
+    # Регулярное выражение для проверки формата email
+    regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    regex.match?(email)
+  end
 end
