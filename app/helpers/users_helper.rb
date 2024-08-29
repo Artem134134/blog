a4bc94bd0d267b8f0368d3dffa870c18005dc6b5
Artem134134 <car322132@gmail.com>
2024-08-29 22:48:27 +0300

    Добавлен метод gravatar в UsersHelper для отображения аватара пользователя; обновлен вызов метода в представлении article

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,11 @@ module UsersHelper
 
     user.email.split('@')[0]
   end
+
+  def gravatar(user, size: 30, css_class: '')
+    email_hash = Digest::MD5.hexdigest(user.email.strip.downcase)
+
+    image_tag "https://www.gravatar.com/avatar/#{email_hash}.jpg?s=#{size}",
+      class: "rounded #{css_class}", alt: username_or_email(user)
+  end
 end
