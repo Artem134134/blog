04b8b41e76d3185a4ae66a47a2ce65de793ab5de
Artem134134 <car322132@gmail.com>
2024-08-29 20:15:47 +0300

    Добавлена ассоциация belongs_to :user в модель Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@
 class Article < ApplicationRecord
   # dependent: :destroy - удаление комментариев связанных со статьёй!
   has_many :comments, dependent: :destroy
+  belongs_to :user
 
   validates :title, presence: true, length: { in: 2..100 }
   validates :text, presence: true, length: { in: 2..1000 }
