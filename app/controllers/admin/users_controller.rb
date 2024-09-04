d020fcd9369e4b3483051996fc37e5a719090ab0
Artem134134 <car322132@gmail.com>
2024-09-04 19:14:28 +0300

    Добавлен метод user params в users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Admin::UsersController < ApplicationController
+	before_action :authenticate_user! # Проверка аутентификации
+
+	def index 
+		@pagy, @users = pagy User.order(created_at: :desc)
+	end
 
 
+	private
+
+	def user_params
+	  params.require(:user).permit(:email, :password, :password_confirmation, :username)
+	end
 end
