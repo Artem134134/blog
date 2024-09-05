fca0f1452fcd8d086932532f8c4dcf5a0084f9c5
Artem134134 <car322132@gmail.com>
2024-09-05 09:56:05 +0300

    Небольшое форматирование users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -26,8 +26,8 @@ class Admin::UsersController < ApplicationController
         User.order(created_at: :desc).each do |user|
           zos.put_next_entry "user_#{user.id}.xlsx"
           zos.print render_to_string(
-            layout: false, handlers: [:axlsx], formats: [:xlsx], template: 'admin/users/user', locals: { user: user }
-          )
+            layout: false, handlers: [:axlsx], formats: [:xlsx],
+             template: 'admin/users/user', locals: { user: user })
         end
       end
 
