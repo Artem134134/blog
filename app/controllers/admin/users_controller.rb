93849350810cc8c01eeb365c39b25ed7666337d5
Artem134134 <car322132@gmail.com>
2024-09-05 16:39:38 +0300

    Добавлен метод respond_with_zipped_users для создания ZIP-архива с данными пользователей в формате XLSX

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,38 +1,39 @@
 # frozen_string_literal: true
 
-class Admin::UsersController < ApplicationController
-	before_action :authenticate_user! # Проверка аутентификации
+module Admin
 
-	def index 
-		respond_to do |format|
-      format.html do
-				@pagy, @users = pagy User.order(created_at: :desc)
-			end
+	class UsersController < ApplicationController
+		before_action :authenticate_user! # Проверка аутентификации
 
-	 		format.zip { respond_with_zipped_users }
-    end
-  end
+		def index 
+			respond_to do |format|
+	      format.html do
+					@pagy, @users = pagy User.order(created_at: :desc)
+				end
+			
+		 		format.zip { respond_with_zipped_users }
+		 	end	
+	  end
 
+		private
 
+		def user_params
+		  params.require(:user).permit(:email, :password, :password_confirmation, :username)
+		end
 
-	private
+		def respond_with_zipped_users
+			compressed_filestream = Zip::OutputStream.write_buffer do |zos|
+	        User.order(created_at: :desc).each do |user|
+	          zos.put_next_entry "user_#{user.username}_#{user.id}.xlsx"
+	          zos.print render_to_string(
+	            layout: false, handlers: [:axlsx], formats: [:xlsx],
+	             template: 'admin/users/user', locals: { user: user })
+	        end
+	      end
 
-	def user_params
-	  params.require(:user).permit(:email, :password, :password_confirmation, :username)
-	end
-
-	def respond_with_zipped_users
-		compressed_filestream = Zip::OutputStream.write_buffer do |zos|
-        User.order(created_at: :desc).each do |user|
-          zos.put_next_entry "user_#{user.id}.xlsx"
-          zos.print render_to_string(
-            layout: false, handlers: [:axlsx], formats: [:xlsx],
-             template: 'admin/users/user', locals: { user: user })
-        end
-      end
-
-      compressed_filestream.rewind
-      send_data compressed_filestream.read, filename: 'users.zip'
-    end
+	      compressed_filestream.rewind
+	      send_data compressed_filestream.read, filename: 'users.zip'
+	    end
 	end
 end
+
