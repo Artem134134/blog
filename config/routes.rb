2265dc445db669976e9d28a8c325ad91890a9f98
Artem134134 <car322132@gmail.com>
2024-09-24 22:26:44 +0300

    Добавлены маршруты для users

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     end
 
     namespace :admin do
-      resources  :users, only: %i[index create edit update destroy]
+      resources  :users
     end
 
     root to: 'articles#index'
