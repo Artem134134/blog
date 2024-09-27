5c97af4e827c21759f3a5b2742eecba34c9d6523
Artem134134 <car322132@gmail.com>
2024-09-27 16:47:00 +0300

    Изменена миграция для удаления значения по умолчанию user_id: добавлена проверка на наличие пользователей

--- db/migrate/20240829115507_remove_default_user_id_from_articles_comments.rb
+++ db/migrate/20240829115507_remove_default_user_id_from_articles_comments.rb
@@ -1,11 +1,10 @@
+# frozen_string_literal: true
+
 class RemoveDefaultUserIdFromArticlesComments < ActiveRecord::Migration[7.1]
-  def up
+  def change
+    return unless User.any?
+
     change_column_default :articles, :user_id, from: User.first.id, to: nil
     change_column_default :comments, :user_id, from: User.first.id, to: nil
-  end
-
-  def down
-    change_column_default :articles, :user_id, from: nil, to: User.first.id
-    change_column_default :comments, :user_id, from: nil, to: User.first.id
   end
 end
