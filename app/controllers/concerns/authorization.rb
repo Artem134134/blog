a00b0b9f8b2428cb14d4232218f2085c250236de
Artem134134 <car322132@gmail.com>
2024-09-27 16:10:21 +0300

    Добавлен модуль Authorization с обработкой ошибок Pundit

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -4,5 +4,14 @@ module Authorization
   included do
     include Pundit::Authorization
 
-  end
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+
+    private
+
+    def user_not_authorized
+      flash[:danger] = t 'global.flash.not_authorized'
+      redirect_to(request.referer || root_path)
+    end
+  end  
 end
