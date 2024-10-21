00c0100474933a9c9f55cd03be340ac5480b72eb
Artem134134 <car322132@gmail.com>
2024-10-21 22:18:01 +0300

    Добавлен метод log_session для логирования сессии и добавлена проверка аутентификации пользователя

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,22 @@ class ApplicationController < ActionController::Base
   include Internationalization
   include Authorization
 
+  before_action :log_session
+
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  
+
+  private
+
+  def log_session
+    Rails.logger.debug "Session: #{session.inspect}"
   end
 end
